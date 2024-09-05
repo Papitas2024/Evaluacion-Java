bbae66a0c98943465fb8fbb61f5e0fad0c9fc618
Papitas2024 <carrascofelipe18@gmail.com>
2024-09-05 13:22:11 -0400

    Mi primer commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+console.log("El script se ha cargado correctamente.");
+
+function mostrarDato() {
+    alert("Blitzcrank fue creado en las profundidades de Zaun como un experimento en la creación de una máquina de guerra que ayudara a asegurar el dominio de los ricos y poderosos de Piltover.");
+}
